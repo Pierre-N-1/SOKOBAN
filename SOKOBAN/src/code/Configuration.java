13936194b7c4fc18709a1c745410ad82a0a68965
arthsv <59049539+arthsv@users.noreply.github.com>
2022-12-05 10:09:38 +0100

    Added trouve_niveau method

--- SOKOBAN/src/code/Configuration.java
+++ SOKOBAN/src/code/Configuration.java
@@ -5,11 +5,12 @@ import java.util.*;
 
 public class Configuration {
 	int m_niveauALancer = -1, m_nbNiveau;
+	List<String> m_listeCodes = new Vector<String>();
 	
 	public Configuration() {
 		String code;
 		int i=0;
-		List<String> m_listeCodes = new Vector<String>(m_nbNiveau);
+		m_listeCodes = new Vector<String>(m_nbNiveau);
 		File file = new File("codes.txt");
 		
 		try {
@@ -25,6 +26,14 @@ public class Configuration {
 	    catch (FileNotFoundException e) {
 	        e.printStackTrace();
 	    }
+	}
+	
+	public int trouve_niveau(String code) {
+		for(int i=0; i<m_nbNiveau; i++){
+	        if (m_listeCodes.get(i) == code)
+	            return i+1;  //On retourne i+1 car la liste commence a l'indice 0
+	    }
+	    return -1;
 	}
 	
 	protected void finalize() {
