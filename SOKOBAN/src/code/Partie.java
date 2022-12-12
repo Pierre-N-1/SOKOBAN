b758646fddc892c689688cdcdb8ecf7f954b2b70
arthsv <59049539+arthsv@users.noreply.github.com>
2022-12-12 10:20:17 +0100

    Update Partie.java

--- SOKOBAN/src/code/Partie.java
+++ SOKOBAN/src/code/Partie.java
@@ -1,4 +1,4 @@
-package code;
+package gui;
 import java.io.*;
 import java.util.Scanner;
 
@@ -15,6 +15,8 @@ public class Partie {
 		m_niv = 1;
 	    m_nbCaisses = 1;
 	    m_c = new Caisse[m_nbCaisses];
+	    m_plat = new Plateau(0, 0, 0, 0);
+	    config = new Configuration();
 	}
 	
 	public void lancerNiveau(int niv){
@@ -25,30 +27,37 @@ public class Partie {
 	    try {
 	    	Scanner sc = new Scanner(file);
 	    	
-	    	m_niv = sc.nextInt();
-	    	m_nbCaisses = sc.nextInt();
-	    	m_plat.m_la = sc.nextInt();
-	    	m_plat.m_lo = sc.nextInt();
+	    	String line = new String();
+	    	//m_plat.m_la = 8;
+	    	//m_plat.m_lo = 1;
+	    	m_niv = Integer.parseInt(sc.nextLine());
+	    	m_nbCaisses = Integer.parseInt(sc.nextLine());
+	    	m_plat.m_la = Integer.parseInt(sc.nextLine());
+	    	m_plat.m_lo = Integer.parseInt(sc.nextLine());
 	    	m_c = new Caisse[m_nbCaisses];
 	    	m_plat.tab = new ObjetImmobile[m_plat.m_la][m_plat.m_lo];
+	    	
 	    	for (int i=0; i<m_plat.m_la; i++){
+	    		m_plat.tab[i] = new ObjetImmobile[m_plat.m_lo];
+	    		line = sc.nextLine();
 	            for (int j=0; j<m_plat.m_lo; j++){
-	                m_plat.tab[i][j].m_type = sc.nextInt();
+	            	//System.out.println(j);
+	            	m_plat.tab[i][j] = new ObjetImmobile(j,i,line.charAt(j));
+	                //m_plat.tab[i][j].m_type = line.charAt(j);
+	                //System.out.println(m_plat.tab[i][j].m_type);
+	                //System.out.println(sc.nextLine().charAt(0));
 
 	                if (m_plat.tab[i][j].m_type == '@' || m_plat.tab[i][j].m_type == '+') {
-	                    m_perso.setX(j);
-	                    m_perso.setY(i);
+	                    m_perso = new Personnage(j,i);
 	                    m_perso.img = ":/images/Joueur/playerDown.png";
 	                }
 	                if (m_plat.tab[i][j].m_type == '$'){
-	                    m_c[k].setX(j);
-	                    m_c[k].setY(i);
+	                	m_c[k] = new Caisse(j,i);
 	                    m_c[k].img = ":/images/Caisse/caisse.png";
 	                    k++;
 	                }
 	                if (m_plat.tab[i][j].m_type == '*'){
-	                    m_c[k].setX(j);
-	                    m_c[k].setY(i);
+	                	m_c[k] = new Caisse(j,i);
 	                    m_c[k].img = ":/images/Caisse/caisseSurCible.png";
 	                    k++;
 	                }
