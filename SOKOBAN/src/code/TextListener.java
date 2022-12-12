d7acad4efbabcccfe48755c38418bd46d9d162f1
arthsv <59049539+arthsv@users.noreply.github.com>
2022-12-12 10:20:57 +0100

    Update TextListener.java

--- SOKOBAN/src/code/TextListener.java
+++ SOKOBAN/src/code/TextListener.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -7,27 +8,43 @@ public class TextListener extends JFrame {
 
 	//créer un JTextField
 	JTextField textField = new JTextField("Appuyez sur Entrée");
+	JTextField errorMsg = new JTextField("Code incorrect");
+	String text = "";
 
-	/**
-	 * Launch the application.
-	 */
+	//Lancement de l'application
 	public static void main(String[] args) {
 		new TextListener().setVisible(true);
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	//Créer la boîte
 	public TextListener() {
 		//ajouter le listener sur le JTextField
 	    textField.addActionListener(new ActionListener() {
 	      //capturer un événement sur le JTextField
 	      public void actionPerformed(ActionEvent e) {
-	    	  String text = textField.getText();
+	    	  text = textField.getText();
 	      }
 	    });
+	    
 	    //ajouter le JTextField au frame
 	    getContentPane().add(textField);
+	    
+	    textField.addKeyListener (new KeyAdapter() {
+	         public void keyPressed(KeyEvent e) {
+	           int key = e.getKeyCode();
+	           if (key == KeyEvent.VK_ENTER) {
+	              Toolkit.getDefaultToolkit().beep();
+	              if (errorMsg.getText() == "") {
+	            	  dispose();
+	              }
+	              else {
+	            	  getContentPane().add(errorMsg);
+	              }
+	              }
+	           }
+	         }
+	      );
+	    
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setLocationRelativeTo(null);
 	    setSize(300,100);
